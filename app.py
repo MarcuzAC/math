a8b8136cb72e7687effa60ba7de0ab2c490c6564
Milton Chithenga <miltonachithenga@gmail.com>
2024-11-08 12:05:58 +0200

    removed Uvicorn

--- app.py
+++ app.py
@@ -124,6 +124,5 @@ def generate_recommendations(input_data):
       #  'recommendations': recommendations
     #})
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(debug=True)
